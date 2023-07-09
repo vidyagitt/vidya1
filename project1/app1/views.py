cb318193a62ec54954836b294819e1f11b423359
vidyasagar <vidyasagar13288@gmail.com>
2023-07-09 15:32:44 +0530

    first commit

---
+++ project1/app1/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def vidya(request):
+    return HttpResponse('vidya is called')
+
+
+def chaitu(request):
+    return HttpResponse('chaitu is called')
+
+
+
+
+def chaitu1(request):
+    return HttpResponse('chaitu1 is called')
+
+
+
+def chaitu2(request):
+    return HttpResponse('chaitu2 is called')
